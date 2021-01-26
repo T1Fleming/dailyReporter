de0124ef5f8133ab9851b56b1e551fe3c5fb8db4
T1Fleming <eric.fleming101@gmail.com>
2021-01-25 19:25:50 -0800

    added csv parser

--- backEnd/service.js
+++ backEnd/service.js
@@ -1,5 +1,40 @@
 const axios = require('axios');
+const fs = require('fs')
+const csv = require('csv-parse');
 require('dotenv').config()
+
+const parseCsv = async function () {
+    //  {
+    //     toDoTask: [ 'toDo', 'refinance car', 'clean kitchen' ],
+    //     workoutTask: [ 'workout', 'chest', 'triceps' ],
+    //     studyTask: [ 'study', 'design backend' ]
+    //   }
+
+    return new Promise((resolve, reject) => {
+        let toDoTask = []
+        let workoutTask = []
+        let studyTask = []
+
+        fs.createReadStream('input.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+
+                // Push Data
+                row[0] != '' ? toDoTask.push(row[0]) : null
+                row[1] != '' ? studyTask.push(row[1]) : null
+                row[2] != '' ? workoutTask.push(row[2]) : null
+
+            })
+            .on('end', () => {
+                console.log('CSV file successfully processed');
+                resolve({
+                    "toDoTask": toDoTask,
+                    "workoutTask": workoutTask,
+                    "studyTask": studyTask
+                })
+            });
+    })
+}
 
 const news = async function () {
     url = 'http://newsapi.org/v2/top-headlines?' +
@@ -42,8 +77,12 @@ const calendar = function () {
 
 
 async function main() {
-    let resp = await news()
-    console.log(resp.data)
+    // let resp = await news()
+    // console.log(resp.data)
+
+    resp = await parseCsv()
+    console.log(resp)
+
 }
 
 main()
